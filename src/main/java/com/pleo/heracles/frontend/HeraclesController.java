1ad5b8569872ccf2b0b4738927d93329e2bb6cc3
MAjchman <majchman@users.noreply.github.com>
2020-07-23 12:14:03 +0200

    Add a converter page
    Added a controller for the converter page and a html-template

---
+++ src/main/java/com/pleo/heracles/frontend/HeraclesController.java
@@ -0,0 +1,13 @@
+package com.pleo.heracles.frontend;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HeraclesController {
+
+    @GetMapping("/converter")
+    public void convert() {
+
+    }
+}
